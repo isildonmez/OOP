7c0fb3be7d6ff0c6f8416de11662c4ae430ae257
Isil Donmez <isil.donmez@gmail.com>
2017-09-24 23:32:21 +0100

    Finish second task

--- tic_tac_toe/tic_tac_toe.rb
+++ tic_tac_toe/tic_tac_toe.rb
@@ -5,22 +5,36 @@ puts "3. Each horizontal line has '3..1' digit name as a coordinate"
 puts "4. Each player should state the coordinate with a letter and a digit, respectively. (e.g. 'a3' or 'C2' and so on.)"
 puts "5. Each player should follow the other"
 
-class Tic_tac_toe
-
+class TicTacToe
   def act
-  	# Memory of each player << gets.chomp.to_sym
+    # Stores the steps of each act as a main method.
+    puts "What is the coordinate of your next act?"
+    @coordinate = gets.chomp.downcase
+    valid_coordinate(@coordinate)
+    # visualise
+    #is_a_winner?
   end
 
-  def memory_of_each_player
-  	#stores each players coordinate as an array of symbols
+  def valid_coordinate(coordinate)
+    # Checks if the coordinate is valid.
+    # I am not sure about this until!
+    until coordinate[0].between?("a","c") && coordinate[1].to_i.between?(1,3)
+      puts "Please state a coordinate with a letter and a digit, respectively. (e.g. 'a3' or 'C2' and so on.)"
+      coordinate = gets.chomp.downcase
+    end
+    coordinate = coordinate.to_sym
+  end
+
+  def memory_of_each_player(player)
+    #stores each players coordinate as an array of symbols
   end
 
   def visualise
-  	# prints the visualised coordinates after each act.
+    # prints the visualised coordinates after each act.
   end
 
-  def is_a_defeat_or_what
-  	# Checks memory_of_each_player if it includes any of the below combinations.
+  def is_a_winner
+    # Checks memory_of_each_player if it includes any of the below combinations.
     @@winners_array = [
       %i[a3 a2 a1], %i[b3 b2 b1], %i[c3 c2 c1],
       %i[a3 b3 c3], %i[a2 b2 c2], %i[a1 b1 c1],
@@ -29,4 +43,14 @@ class Tic_tac_toe
 
   end
 
+end
+
+X = TicTacToe.new
+O = TicTacToe.new
+
+until X.is_a_winner || O.is_a_winner
+  for i in 0...10
+    X.act if i.odd?
+    Y.act if i.even?
+  end
 end
