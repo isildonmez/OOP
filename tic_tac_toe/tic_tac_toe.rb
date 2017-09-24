8e6225cdca2471de531e45229f83f7261ec64561
Isil Donmez <isil.donmez@gmail.com>
2017-09-24 01:03:29 +0100

    Set up the rules

---
+++ tic_tac_toe/tic_tac_toe.rb
@@ -0,0 +1,7 @@
+puts "Rules:"
+puts "1. Each horizontal line has 'a..c' letter name as a coordinate"
+puts "2. Each vertical line has '1..3' digit name as a coordinate"
+puts "3. Each player should state the coordinate with a letter and a digit, respectively. (e.g. 'a3' or 'C2' and so on.)"
+puts "4. Each player should follow the other"
+class Tic_tac_toe
+end
