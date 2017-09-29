65639f9e835e6da6fb8daf57b1af855dcccee8ac
Isil Donmez <isil.donmez@gmail.com>
2017-09-29 15:27:43 +0100

    Change some styling

--- master_mind/master_mind.rb
+++ master_mind/master_mind.rb
@@ -1,34 +1,50 @@
 class Mastermind
   def initialize()
+    puts "Welcome to Mastermind. Who do you want to be?"
+    @proposition = "Please write 'breaker' if you want to be the code breaker or 'maker' to be the code maker."
+    puts @proposition
+    @request = gets.chomp.downcase
+    puts @request
+
+    which_player
     which_game
   end
 
   def which_player
-    puts "Welcome to Mastermind. Who do you want to be?"
-    proposition = "Please write 'breaker' if you want to be the code breaker or 'maker' to be the code maker."
-    puts proposition
-    request = gets.chomp.downcase
-    until request = "breaker " || "maker"
-      puts proposition
-      request = gets.chomp.downcase
+    until @request == "breaker" || @request == "maker"
+      puts @proposition
+      @request = gets.chomp.downcase
     end
-    return request
   end
 
   def which_game
-    if which_player == "breaker"
-      @code_breaker = CodeBreaker.new
-    else
-      @code_maker = CodeMaker.new
-    end
+    @request == "breaker" ? @code_breaker = CodeBreaker.new : @code_maker = CodeMaker.new
+
+    # case @request
+    #   when "breaker"
+    #   	@code_breaker = CodeBreaker.new
+    #   else
+    #   	@code_maker = CodeMaker.new
+    # end
+
+    # if @request == "breaker"
+    #   @code_breaker = CodeBreaker.new
+    # else
+    #   @code_maker = CodeMaker.new
+    # end
   end
 end
 
 class CodeBreaker
-
+  def initialize()
+    puts "Breaker Babe"
+  end
 end
 
 class CodeMaker
+  def initialize()
+    puts "Maker Yoo"
+  end
 end
 
 game = Mastermind.new
